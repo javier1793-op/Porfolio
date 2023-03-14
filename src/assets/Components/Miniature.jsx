9e42327ee48ee529e0fe6691db97ad9ab5b8ce43
NicolasAyal <javier17utn@gmail.com>
2023-03-14 13:06:39 -0300

    miniaturas

---
+++ src/assets/Components/Miniature.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../Css/miniature.scss'
+import Foto from "./../Img/perfil-miniatura.jpg";
+
+const Miniature = ({git, page, img}) => {
+  return (
+    <div className="project-img">
+    <img src={img} alt="miniatura" className='portada'/>
+    <div className="detall">
+      <button>
+        <a href={git} target="_blank"><i class="fa-brands fa-github"></i>Code</a>
+      </button>
+      <button>
+        <a href={page} target="_blank">  <i class="fa-solid fa-laptop"></i>Page</a>
+      </button>
+    </div>
+  </div>
+  )
+}
+
+export default Miniature
--- src/assets/Page/Projetc.jsx
+++ src/assets/Page/Projetc.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Card from "../Components/Card";
+import Miniature from "../Components/Miniature";
 import "./../Css/project.scss";
-import Foto from "./../Img/perfil-miniatura.jpg";
+import Loading from '../Img/project/Component/loading-sr.png'
+
 
 const Projetc = () => {
   return (
@@ -12,17 +14,7 @@ const Projetc = () => {
             <div className="section-title padd-15">
               <h2>Projects</h2>
               <div className="gallery">
-                <div className="project-img">
-                  <img src={Foto} alt="" />
-                  <div className="detall">
-                    <button>
-                      <i class="fa-brands fa-github"></i>Code
-                    </button>
-                    <button>
-                      <i class="fa-solid fa-laptop"></i>Page
-                    </button>
-                  </div>
-                </div>
+              
               </div>
             </div>
           </div>
@@ -30,16 +22,18 @@ const Projetc = () => {
           <div className="row">
             <div className="section-title padd-15">
               <h2>Challenges</h2>
+              <div className="gallery">
+               
+              </div>
             </div>
           </div>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
           <div className="row">
             <div className="section-title padd-15">
               <h2>Components</h2>
+              <div className="gallery">
+                <Miniature git='https://github.com/javier1793-op/Loading.git' page='https://javier1793-op.github.io/Loading/' img={Loading}/>
+              
+              </div>
             </div>
           </div>
         </div>
